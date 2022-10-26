7ee6c1bb680d42cc3dcf207bb88b40b8f3a88c30
Mike Teslin <3131868@gmail.com>
2022-10-26 19:56:27 +0300

    ready 1 day

---
+++ src/day1/Task4.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println("Мой возраст " + age);
+
+        while(age <= 100) {
+            age = age + 5;
+            System.out.println("Мой возраст " + age);
+        }
+    }
+}
